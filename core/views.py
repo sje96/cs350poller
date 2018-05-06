4f4b53cd1c26b531c983429b7bf5563d6629f04c
sje96 <stevenesquibel@hotmail.com>
2018-05-06 16:48:57 -0600

    sesquibel
    finished

--- core/views.py
+++ core/views.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import Context, loader
 
 # Create your views here.
+
+def index(request):
+    template = loader.get_template('home.html') 
+    return HttpResponse(template.render())
--- mysite/urls.py
+++ mysite/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-
+from core import views
 
 urlpatterns = [
+    url(r'^$', views.index, name='index'),
     url(r'^polls/', include('polls.urls')),
     url(r'^admin/', admin.site.urls),
+
 ]
